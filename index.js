b5ca886fff5e0df354373fbeb5d30e4e278b2d3e
berlinum <maks.filimonov@gmail.com>
2020-04-08 22:59:51 +0200

    Connect to MongoDB using mongoose

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+async function start() {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log('Server error', error.message);
+    process.exit(1);
+  }
+}
+start();
+
+// Serve any static files
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`);
 });
