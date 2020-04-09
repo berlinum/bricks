f3ca49c9c07b4bf4d11391eee667b14254858052
berlinum <maks.filimonov@gmail.com>
2020-04-09 15:51:41 +0200

    Add draft Input component

---
+++ client/src/components/Input.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const Input = styled.input`
+  min-width: 280px;
+  height: 46px;
+  background-color: #f5f5f5;
+  color: #1d1d1b;
+  border: none;
+  border-radius: 5px;
+  padding-left: 15px;
+  text-align: left;
+`;
+
+export default Input;
