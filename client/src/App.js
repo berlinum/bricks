f9a8435c555bffc44b8597825820fa9a6603e049
berlinum <maks.filimonov@gmail.com>
2020-04-09 16:04:39 +0200

    Add styled Input components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Input from './components/Input';
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 375px;
+  height: 812px;
+  background-color: #1d1d1b;
+`;
 
 function App() {
-  return <div>BRICKS</div>;
+  return (
+    <Card>
+      <Input placeholder="Email" />
+      <Input placeholder="Password" />
+    </Card>
+  );
 }
 
 export default App;
--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 const Input = styled.input`
+  margin: 10px 0;
   min-width: 280px;
   height: 46px;
   background-color: #f5f5f5;
@@ -9,6 +10,7 @@ const Input = styled.input`
   border-radius: 5px;
   padding-left: 15px;
   text-align: left;
+  font-size: 20px;
 `;
 
 export default Input;
