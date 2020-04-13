98f72f57bea366d93a57785b8c4408f1bf5ba4d9
Maxim <maks.filimonov@gmail.com>
2020-04-13 13:11:16 +0200

    Connect to MongoDB using mongoose (#8)
    * Create file with environment variables
    
    * Connect to MongoDB using mongoose
    
    * Add route for user-authentication
    
    * Add draft User model
    
    * Add email validation for a new user
    
    * Add npm bcryptjs for password encryption
    
    * Create a new user with hashed password
    
    * Add express-validator to dependencies
    
    * Add validation to email and password
    
    * Remove .env from git
    
    * Rm .env file
    
    * Update index.js
    
    Co-Authored-By: Leon Machens <leon.machens@googlemail.com>
    
    Co-authored-by: Leon Machens <leon.machens@googlemail.com>

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const auth = require('./lib/routes/auth');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use('/api/auth', auth);
+
+async function start() {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log('Server error', error.message);
+    process.exit(1);
+  }
+}
+start();
+
+// Serve any static files
 app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
-app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`);
 });
