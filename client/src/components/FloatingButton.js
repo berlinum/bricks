d371a643468e98ebb0bee581caba4add04bdb450
berlinum <maks.filimonov@gmail.com>
2020-04-16 23:37:22 +0200

    Add floating button

--- client/src/components/FloatingButton.js
+++ client/src/components/FloatingButton.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import Plus from '../assets/icons/whitePlus.svg';
 import colors from '../utils/colors';
 
-export const AddButton = styled.button`
+const AddButton = styled.button`
   width: 70px;
   height: 70px;
   border: none;
@@ -12,13 +14,20 @@ export const AddButton = styled.button`
   background-repeat: no-repeat;
   background-position: center;
   box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
+  transform: rotate(${(props) => (props.active ? 45 : 0)}deg);
+  transition: 0.5s;
   &:focus {
     outline: none;
     transform: rotate(45deg);
     transition: 0.5s;
   }
-  &:active {
-    transform: rotate(45deg);
-    transition: 0.5s;
-  }
 `;
+
+export const FloatingButton = ({ value, onButtonClick }) => (
+  <AddButton active={value} onClick={() => onButtonClick()} />
+);
+
+FloatingButton.propTypes = {
+  value: PropTypes.bool,
+  onButtonClick: PropTypes.func,
+};
---
+++ client/src/stories/FloatingButton.stories.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { FloatingButton } from '../components/FloatingButton';
+
+export default {
+  title: 'Button',
+  component: FloatingButton,
+};
+
+export const AnimatedFloatingButton = () => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <FloatingButton value={active} onButtonClick={() => setActive(!active)} />
+  );
+};
