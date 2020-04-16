267845769ac471d37c78572f0c14dcd6893a3e03
berlinum <maks.filimonov@gmail.com>
2020-04-16 12:27:07 +0200

    Add buttom nav bar

---
+++ client/src/stories/NavBar.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+export default {
+  title: 'Navigation',
+  component: NavBar,
+};
+
+export const NavigationBar = () => <NavBar />;
