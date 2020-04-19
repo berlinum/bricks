dff7128be837a25a7884a5b7b2ec62e39dc66da5
berlinum <maks.filimonov@gmail.com>
2020-04-19 17:13:02 +0200

    Add header components

---
+++ client/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import colors from '../utils/colors';
+
+export const HeaderBox = styled.div`
+  display: flex;
+  height: 100px;
+  width: 100%;
+  background-color: ${colors.bgDark};
+`;
+
+export const TtileBox = styled.div`
+  font-family: SF Pro Display Semibold;
+  font-size: 30px;
+  color: ${colors.textActiv};
+  margin: 44px 0 0 30px;
+`;
+
+export const Header = () => (
+  <HeaderBox>
+    <TtileBox>Collection</TtileBox>
+  </HeaderBox>
+);
+
+export default Header;
