79c7a920de7ad57c42728c2f6c875a74ee0bd2cd
berlinum <maks.filimonov@gmail.com>
2020-04-23 19:41:38 +0200

    Add the draft main page with logout button

--- client/src/pages/CollectionPage.js
+++ client/src/pages/CollectionPage.js
@@ -2,6 +2,19 @@ import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../components/Button';
 import { AuthContext } from '../context/AuthContext';
+import Header from '../components/Header';
+import styled from '@emotion/styled';
+import CardItem from '../components/CardItem';
+import Img from '../assets/img/Bus.jpg';
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-flow: row wrap;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+  overflow: scroll;
+`;
 
 const CollectionPage = () => {
   const history = useHistory();
@@ -12,10 +25,52 @@ const CollectionPage = () => {
     history.push('/');
   };
   return (
-    <div>
-      <h1>Collection Page</h1>
-      <Button onClick={logoutHandler}>Log Out</Button>
-    </div>
+    <>
+      <Header title="Collection" />
+      <MainContainer>
+        <CardItem
+          details={{
+            id: 123,
+            title: 'London Bus',
+            pieces: 1686,
+            item: 10258,
+            year: 2017,
+            img: Img,
+          }}
+        />
+        <CardItem
+          details={{
+            id: 123,
+            title: 'London Bus',
+            pieces: 1686,
+            item: 10258,
+            year: 2017,
+            img: Img,
+          }}
+        />
+        <CardItem
+          details={{
+            id: 123,
+            title: 'London Bus',
+            pieces: 1686,
+            item: 10258,
+            year: 2017,
+            img: Img,
+          }}
+        />
+        <CardItem
+          details={{
+            id: 123,
+            title: 'London Bus',
+            pieces: 1686,
+            item: 10258,
+            year: 2017,
+            img: Img,
+          }}
+        />
+        <Button onClick={logoutHandler}>Log Out</Button>
+      </MainContainer>
+    </>
   );
 };
 
