bc3e8089d982afae2071f37a750ee7ba6f3a7f2d
berlinum <maks.filimonov@gmail.com>
2020-04-25 22:27:06 +0200

    Add search input component

---
+++ client/src/components/SearchInput.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import colors from '../utils/colors';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  display: flex;
+  height: 46px;
+  width: 100vw;
+  justify-content: center;
+  background: ${colors.textActive};
+`;
+
+const Input = styled.input`
+  display: flex;
+  width: 100%;
+  min-width: 315px;
+  max-width: 415px;
+  margin: 8px 30px;
+  padding: 0 8px;
+  border-radius: 6px;
+  border: none;
+  outline: none;
+  font-family: 'SF Pro Text Regular';
+  font-size: 16px;
+  color: ${colors.textPrimary};
+  &::placeholder {
+    color: ${colors.textInfo};
+  }
+`;
+
+const SearchInput = ({ value, onChange }) => {
+  return (
+    <div>
+      <Container>
+        <Input
+          type="search"
+          placeholder="Search by name or set number"
+          value={value}
+          onChange={onChange}
+        ></Input>
+      </Container>
+    </div>
+  );
+};
+
+SearchInput.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  searchResults: PropTypes.array,
+};
+
+export default SearchInput;
