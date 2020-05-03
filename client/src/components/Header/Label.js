bdf152508b71e9b7f4e0c9111d53b290d29d61d1
berlinum <maks.filimonov@gmail.com>
2020-05-03 23:14:41 +0200

    Change header components

---
+++ client/src/components/Header/Label.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const Label = styled.span`
+  font-family: SF Pro Text Regular;
+  font-size: 18px;
+  margin: 5px;
+`;
+export default Label;
---
+++ client/src/components/Header/Title.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import colors from '../../utils/colors';
+
+const Title = styled.div`
+  position: relative;
+  font-family: SF Pro Display Semibold;
+  font-size: 30px;
+  flex-grow: 1;
+  color: ${colors.textActive};
+  margin: 30px 0 0 30px;
+`;
+
+export default Title;
