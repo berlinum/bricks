b1d08d60f1f0c912770d09109cd03fb9cc9ac6cb
berlinum <maks.filimonov@gmail.com>
2020-05-08 23:25:06 +0200

    Add favorite icon

---
+++ client/src/components/Favorite.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Favorite from '../assets/icons/Favorite';
+import PropTypes from 'prop-types';
+
+const FavIcon = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 15px;
+  cursor: pointer;
+  z-index: 1;
+`;
+
+const FavoriteIcon = ({ onFavClick, active }) => (
+  <FavIcon onClick={onFavClick}>
+    <Favorite active={active} />
+  </FavIcon>
+);
+
+FavoriteIcon.propTypes = {
+  id: PropTypes.string,
+  active: PropTypes.bool,
+  onFavClick: PropTypes.func,
+};
+
+export default FavoriteIcon;
